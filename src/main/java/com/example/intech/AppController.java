cd990fb5c5d7f868d0cca71c4f643633babfd228
Valentina <valentina@2lemetry.ru>
2019-09-19 20:17:19 +0300

    INTECH-1

---
+++ src/main/java/com/example/intech/AppController.java
@@ -0,0 +1,23 @@
+package com.example.intech;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AppController {
+
+//    @GetMapping("/greeting")
+//    public String greeting(
+//            @RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+//        model.addAttribute("name", name);
+//        return "greeting";
+//    }
+
+    @GetMapping
+    public String index(Model model) {
+        model.addAttribute("some", "i love you Dima!");
+        return "app-template";
+    }
+}
